4eb5c1dc701f4617f1df526de31b8df69c87c615
NancyAlo <cinepro500@gmail.com>
2024-07-21 21:30:18 -0600

    Redes Sociales

---
+++ scripts.js
@@ -0,0 +1,15 @@
+function ShowHide(){
+     var container = document.getElementsByClassName("menu-vertical")[0];
+     
+     if (container.style.visibility == "hidden"){
+        container.style.visibility = "visible";
+     }else{
+        container.style.visibility = "hidden";
+
+
+     
+
+     }
+     }
+
+
